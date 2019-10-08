c43355f859fa7b7c9b9ff8c7a4fb72dc9821ef69
SunnyWinteri <2218443280@qq.com>
2019-10-09 03:17:09 +1400

    add new Insertion to master

--- InsertionSort.h
+++ InsertionSort.h
@@ -5,9 +5,22 @@
 
 template<typename T>
 void insertionSort(T arr[], int n){
-	for(int i = 0 ; i < n ; i ++)
+
+/*
+	for(int i = 1 ; i < n ; i ++)
 		for(int j = i ; j > 0 && arr[j] < arr[j-1] ; j --)
 			swap(arr[j], arr[j-1]);
+*/
+
+	// 改进版
+	for(int i = 1; i < n; i++){
+		T e = arr[i]; // 拷贝一份当前的数组的值，寻找元素arr[i]合适的插入位置
+		int j; // j保存元素e应该插入的位置
+		for(j = i; j > 0 && arr[j-1] > e; j --)
+			arr[j] = arr[j-1];
+		arr[j] = e;
+	}
+		
 }
 
 
