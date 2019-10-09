8bce14e3e609b91b26ffaa451cf09e26b8f86acf
SunnyWinteri <2218443280@qq.com>
2019-10-09 15:03:54 +0800

    add QuickSort

--- main.cpp
+++ main.cpp
@@ -2,8 +2,9 @@
 #include "SelectionSort.h"
 #include "InsertionSort.h"
 #include "BubbleSort.h"
+#include "QuickSort.h"
 int main(){
-	int n = 10000;
+	int n = 20000;
 	int *arr = SortTestHelper::generateRandomArray(n, 0, n);
 	int *arr2 = SortTestHelper::copyIntArray(arr, n);
 	int *arr3 = SortTestHelper::copyIntArray(arr, n);
@@ -16,6 +17,14 @@ int main(){
 	SortTestHelper::testSort("Bubble Sort 2",bubbleSort2,arr4,n);
 	//SortTestHelper::testSort("Bubble Sort 3",bubbleSort3,arr5,n);
 	//SortTestHelper::testSort("Bubble Sort 4",bubbleSort4,arr6,n);
+	SortTestHelper::testSort("Quick Sort", quickSort, arr5, n);
+
+	delete[] arr;
+	delete[] arr2;
+	delete[] arr3;
+	delete[] arr4;
+	delete[] arr5;
+	delete[] arr6;
 	return 0;
 }
 
